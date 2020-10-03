a23ee27f02ddf7497938b33281ed782e80d03c05
egipe <gipe.elisa@gmail.com>
2020-10-03 14:38:10 -0700

    made some of it work!

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -29,7 +29,7 @@ var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.ge
 
 // Define a markerSize function that will give each earthquake size based on magnitude
 function markerSize(magnitude) {
-  return magnitude * 1000;
+  return magnitude * 20000;
 }
 
 // Define markerColor function that will give each earthquake color based on depth
@@ -38,71 +38,49 @@ function markerSize(magnitude) {
 // use d3 to get data
 d3.json(url).then(function(data){
 
-  // was instructed to do features.on but could not figure it out within the timeframe
-  L.geoJson(data, {
-    // We turn each feature into a circleMarker on the map.
-    pointToLayer: function(feature, latlng) {
-      return L.circleMarker(latlng);
-    },
-    // We set the style for each circleMarker using our styleInfo function.
-    style: styleInfo,
-    // We create a popup for each marker to display the magnitude and location of the earthquake after the marker has been created and styled
-    onEachFeature: function(feature, layer) {
-      layer.bindPopup("Magnitude: "
-          + feature.properties.mag
-          + "<br>Depth: "
-          + feature.geometry.coordinates[2]
-          + "<br>Location: "
-          + feature.properties.place
-      );
-    }
-  }).addTo(map);
-
   // Loop through locations and create city and state markers
+  //console.log(data.features.length)
   for (var i = 0; i < data.features.length; i++) {
+    console.log(data.features.length)
     // declare variables
-    var lat = data[i].features.geometry.coordinates[1];
-    var lon = data[i].features.geometry.coordinates[0];
+    var lat = data.features[i].geometry.coordinates[1];
+    var lon = data.features[i].geometry.coordinates[0];
     var latlon = [lat,lon];
-    var mag = data[i].features.properties.mag;
-    var depth = data[i].features.properties.geometry.coordinates[2];
-    var place = data[i].features.properties.place;
+    var mag = data.features[i].properties.mag;
+    var depth = data.features[i].geometry.coordinates[2];
+    var place = data.features[i].properties.place;
+    console.log(place)
 
     var fill = "";
     if (depth > 90) {
-      return "red"
+      fill = "red"
     }
-    else if (depth2 > 70) {
-      return "dark orange"
+    else if (depth > 70) {
+      fill = "orange"
     }
-    else if (depth > 50) {
-      return "light orange"
-    }
+    // else if (depth > 50) {
+    //   fill = "light orange"
+    // }
     else if (depth > 30) {
-      return "yellow"
+      fill = "yellow"
     }
-    else if (depth > 10) {
-      return "light green"
-    }
+    // else if (depth > 10) {
+    //   fill = "light green"
+    // }
     else {
-      return "dark green"
+      fill = "green"
     }
-
 
     //Setting the marker radius 
-    L.circle(latlon[i], {
+    L.circle(latlon, {
       stroke: false,
       fillOpacity: 0.75,
-      color: "fill",
-      // fillColor: "fill",
-      //markerColor(depth2),
+      color: fill,
+      fillColor: fill,
       radius:markerSize(mag)
-      .addto(myMap)
-    })
+    }).addTo(myMap)
   }
 });
-
-// control layer
 
 // create the legend 
 // Update the legend's innerHTML with the last updated time and station count
