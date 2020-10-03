198f5bb4682021893845c7cc48c006fca007fd8a
egipe <gipe.elisa@gmail.com>
2020-10-02 23:52:36 -0700

    updating repo

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -21,11 +21,75 @@ var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{
   accessToken: API_KEY
 });
 
-// Markers
+// Add our 'lightmap' tile layer to the map
+lightmap.addTo(myMap);
 
 // call API
 var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson";
 
-// use d3 to get data
+// Define a markerSize function that will give each earthquake size based on magnitude
+function markerSize(magnitude) {
+  return magnitude * 1000;
+}
 
-// loop through earthquakes
+// Define markerColor function that will give each earthquake color based on depth
+function markerColor(depth){
+  if depth > 90 {
+    return red
+  }
+  else if depth > 70 {
+    return dark orange
+  }
+  else if depth > 50 {
+    return light orange
+  }
+  else if depth > 30 {
+    return yellow
+  }
+  else if depth > 10 {
+    return light green
+  }
+  else {
+    return dark green
+  }
+  
+}
+
+// use d3 to get data
+d3.json(url, function(data){
+
+// Loop through locations and create city and state markers
+for (var i = 0; i < data.features.length; i++) {
+    // declare variables
+    var latitude
+    var longitude
+    var magnitude
+    var depth
+    
+  //Setting the marker radius for the state by passing population into the markerSize function
+  stateMarkers.push(
+    L.circle(locations[i].coordinates, {
+      stroke: false,
+      fillOpacity: 0.75,
+      color: "white",
+      // markerColor
+      fillColor: "white",
+      radius: markerSize(locations[i].state.population)
+    })
+  );
+})
+
+// control layer
+
+// create the legend 
+// Update the legend's innerHTML with the last updated time and station count
+// function updateLegend(time, stationCount) {
+//   document.querySelector(".legend").innerHTML = [
+//     "<p>Updated: " + moment.unix(time).format("h:mm:ss A") + "</p>",
+//     "<p class='out-of-order'>Out of Order Stations: " + stationCount.OUT_OF_ORDER + "</p>",
+//     "<p class='coming-soon'>Stations Coming Soon: " + stationCount.COMING_SOON + "</p>",
+//     "<p class='empty'>Empty Stations: " + stationCount.EMPTY + "</p>",
+//     "<p class='low'>Low Stations: " + stationCount.LOW + "</p>",
+//     "<p class='healthy'>Healthy Stations: " + stationCount.NORMAL + "</p>"
+//   ].join("");
+// }
